6e9c6caf5ef71fc0d0db3df136c1ae1757367529
MJ <mj.isip123@gmail.com>
2018-03-10 22:37:12 +0800

    first commit

---
+++ CALCULATOR/ViewControllerModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerModel.swift
+//  CALCULATOR
+//
+//  Created by Admin (TIMOTHY) on 05/01/2018.
+//  Copyright © 2018 Admin (TIMOTHY). All rights reserved.
+//
+
+import Foundation
